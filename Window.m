f01a46fb5fe5e277222220c5fb3084e529537868
Mark Cafaro <markcafaro@d-128-95-10-211.dhcp4.washington.edu>
2014-02-04 15:43:55 -0800

    Added flipping event to Window

--- Window.m
+++ Window.m
@@ -6,6 +6,10 @@ classdef Window < handle
         handle
     end
     
+    events
+        flipping
+    end
+    
     methods
         
         % Constructs a window with the optionally provided size. By default the window occupies the fullscreen of the
@@ -64,6 +68,7 @@ classdef Window < handle
         end
         
         function flip(obj)
+            notify(obj, 'flipping');
             glfwSwapBuffers(obj.handle);
         end
         
