64ef8301a21520f279a88140e3bc0656ec2cc644
Mark Cafaro <mark.cafaro@gmail.com>
2015-09-28 12:47:09 -0700

    Added Stage project for packaging

---
+++ package.m
@@ -0,0 +1,12 @@
+function package(skipTests)
+    if nargin < 1
+        skipTests = false;
+    end
+    
+    if ~skipTests
+        test();
+    end
+    root = fileparts(mfilename('fullpath'));
+    matlab.apputil.package(fullfile(root, 'Stage.prj'));
+end
+
---
+++ test.m
@@ -0,0 +1,14 @@
+function test(package)
+    if nargin < 1
+        package = 'stage';
+    end
+    addpath(genpath(fullfile('src', 'test')));
+    suite = matlab.unittest.TestSuite.fromPackage(package, 'IncludingSubpackages', true);
+    results = run(suite);
+    
+    failed = sum([results.Failed]);
+    if failed > 0
+        error([num2str(failed) ' test(s) failed!']);
+    end
+end
+
