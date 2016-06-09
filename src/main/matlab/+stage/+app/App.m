96a72987b181b210cd9fc3d43dd437180abb21fd
Mark Cafaro <mark.cafaro@gmail.com>
2016-06-09 16:31:17 -0700

    Updated version number

--- src/main/matlab/+stage/+app/App.m
+++ src/main/matlab/+stage/+app/App.m
@@ -11,7 +11,7 @@ classdef App < handle
         end
         
         function v = version()
-            v = '2.0.3.2'; % i.e. 2.0-r2
+            v = '2.0.3.3'; % i.e. 2.0-r3
         end
         
         function o = owner()
