a8a4ae4398d48c7f0541ea65af3bbbc9c2155f8b
Mark Cafaro <mark.cafaro@gmail.com>
2016-06-09 15:03:42 -0700

    Updated stage server app

--- src/main/matlab/+stage/+app/App.m
+++ src/main/matlab/+stage/+app/App.m
@@ -2,8 +2,24 @@ classdef App < handle
     
     methods (Static)
         
+        function n = name()
+            n = 'Stage';
+        end
+        
+        function d = description()
+            d = 'Visual Stimulus System';
+        end
+        
         function v = version()
             v = '2.0.3.2'; % i.e. 2.0-r2
+        end
+        
+        function o = owner()
+            o = 'Stage-VSS';
+        end
+        
+        function r = repo()
+            r = 'stage2';
         end
         
     end
