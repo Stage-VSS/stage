5c007844cf018a72bf08ab1b85db0bde16144174
Mark Cafaro <mark.cafaro@gmail.com>
2016-07-30 16:22:26 -0700

    Updated for font and busy presenter changes

--- src/main/resources/symphony/+io/+github/+stage_vss/+figures/FrameTimingFigure.m
+++ src/main/resources/symphony/+io/+github/+stage_vss/+figures/FrameTimingFigure.m
@@ -1,39 +1,37 @@
 classdef FrameTimingFigure < symphonyui.core.FigureHandler
-    
+
     properties (SetAccess = private)
         device
     end
-    
+
     properties (Access = private)
         axesHandle
         sweep
     end
-    
+
     methods
-        
+
         function obj = FrameTimingFigure(device)
             obj.device = device;
-            
+
             obj.createUi();
         end
-        
+
         function createUi(obj)
             obj.axesHandle = axes( ...
                 'Parent', obj.figureHandle, ...
-                'FontName', get(obj.figureHandle, 'DefaultUicontrolFontName'), ...
-                'FontSize', get(obj.figureHandle, 'DefaultUicontrolFontSize'), ...
                 'XTickMode', 'auto');
             xlabel(obj.axesHandle, 'flip');
             ylabel(obj.axesHandle, 'sec');
-            
+
             obj.setTitle([obj.device.name ' Frame Timing']);
         end
-        
+
         function setTitle(obj, t)
             set(obj.figureHandle, 'Name', t);
             title(obj.axesHandle, t);
         end
-        
+
         function handleEpoch(obj, epoch) %#ok<INUSD>
             info = obj.device.getPlayInfo();
             if isa(info, 'MException')
@@ -53,8 +51,7 @@ classdef FrameTimingFigure < symphonyui.core.FigureHandler
                 set(obj.sweep, 'XData', x, 'YData', y);
             end
         end
-        
-    end
-    
-end
 
+    end
+
+end
